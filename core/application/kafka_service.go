eb6af13c1e9ce349d09d8825666ad1a30ec0a656
William Horst <williamhorst@Williams-MacBook-Pro.local>
2025-12-03 12:52:32 -0500

    Get correlation Id

--- core/application/kafka_service.go
+++ core/application/kafka_service.go
@@ -34,7 +34,7 @@ func (s *KafkaService) HandleRequest(req domain.Request) (domain.Response, error
 	}
 	fmt.Println("Found correlation Id: ", correlationId)
 
-	responseData := []byte{00, 00, 00, 00, req.Data[4], req.Data[5], req.Data[6], req.Data[7]}
+	responseData := []byte{00, 00, 00, 00, req.Data[8], req.Data[9], req.Data[10], req.Data[11]}
 
 	fmt.Printf("Response Data %+v", responseData)
 	return domain.Response{
