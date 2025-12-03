0124dc51fcdc4215d53d5dc4e65cd417ca623b9a
William Horst <williamhorst@Williams-MacBook-Pro.local>
2025-12-03 18:36:12 -0500

    Remove Hard Coded Tag Buffer Limit

--- core/application/kafka_service.go
+++ core/application/kafka_service.go
@@ -42,7 +42,7 @@ func (s *KafkaService) HandleRequest(req domain.Request) (domain.Response, error
 		MinVersion:         []byte{0x00, 0x00},
 		MaxVersion:         []byte{0x00, 0x04},
 		ThrottleTimeMs:     []byte{0x00, 0x00, 0x00, 0x00},
-		TagBufferParent:    []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+		TagBufferParent:    []byte{0x00},
 	}
 
 	// Encode the response using the protocol parser (infrastructure concern)
@@ -61,6 +61,8 @@ func (s *KafkaService) HandleRequest(req domain.Request) (domain.Response, error
 func (s *KafkaService) determineErrorCode(apiVersion int) []byte {
 	fmt.Println("Api Version Sent is: ", apiVersion)
 	errorCodeBuffer := []byte{0x00, 0x00}
+
+	//Hard Code for `Handle ApiVersions requests`
 	if apiVersion == 1024 {
 		return errorCodeBuffer
 	}
