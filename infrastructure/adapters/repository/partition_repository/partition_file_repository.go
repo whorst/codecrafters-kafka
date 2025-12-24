f65d2cdf40e535c92b7988c8c9683bc32483ad6d
William Horst <williamhorst@Williams-MacBook-Pro.local>
2025-12-24 15:51:30 -0500

    Remove Debug

--- infrastructure/adapters/repository/partition_repository/partition_file_repository.go
+++ infrastructure/adapters/repository/partition_repository/partition_file_repository.go
@@ -2,9 +2,7 @@ package partition_file_repository
 
 import (
 	"fmt"
-	"io/fs"
 	"os"
-	"path/filepath"
 	"strconv"
 
 	"github.com/codecrafters-io/kafka-starter-go/core/domain"
@@ -27,26 +25,7 @@ func (PartitionFileRepository) GetPartitionMessage(messageFetchRequest domain.Me
 
 func openLogFile(partitionToFetch domain.PartitionToFetch) {
 
-	root := "/tmp/kraft-combined-logs/"
-
-	// WalkDir is the most efficient way to recursively traverse directories in Go.
-	// It visits every file and folder starting from the root path.
-	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
-		// If there is an error accessing a path (e.g., permissions), we report it
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error accessing %s: %v\n", path, err)
-			return nil // Return nil to continue walking other directories
-		}
-
-		// We only want to print files, not the directory names themselves
-		if !d.IsDir() {
-			fmt.Println(path)
-		}
-
-		return nil
-	})
-
-	fileToFetch := fmt.Sprintf("/tmp/kraft-combined-logs/%s-%s/00000000000000000000", partitionToFetch.TopicName, strconv.Itoa(partitionToFetch.PartitionIndex))
+	fileToFetch := fmt.Sprintf("/tmp/kraft-combined-logs/%s-%s/00000000000000000000.log", partitionToFetch.TopicName, strconv.Itoa(partitionToFetch.PartitionIndex))
 
 	fmt.Println(">>>>>>>>> Attempting to fetch file", fileToFetch)
 
