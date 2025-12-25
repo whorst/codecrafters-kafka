17236962caa440d3ab2340b4a4b0b851869f6711
William Horst <williamhorst@Williams-MacBook-Pro.local>
2025-12-24 20:23:04 -0500

    Remove record length

--- infrastructure/adapters/parser/kafka_protocol_parser_fetch.go
+++ infrastructure/adapters/parser/kafka_protocol_parser_fetch.go
@@ -386,9 +386,9 @@ func (p *KafkaProtocolParserFetch) EncodeResponse(response *domain.ResponseDataF
 			binary.BigEndian.PutUint32(preferredReadReplicaBytes, uint32(partition.PreferredReadReplica))
 			responseData = append(responseData, preferredReadReplicaBytes...)
 
-			recordLengthVarInt := common.IntToVarInt(73)
-
-			responseData = append(responseData, recordLengthVarInt...)
+			//recordLengthVarInt := common.IntToVarInt(73)
+			//
+			//responseData = append(responseData, recordLengthVarInt...)
 
 			// Records (variable length - just append the bytes)
 			responseData = append(responseData, partition.Records...)
